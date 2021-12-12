f4e6a0a62144c00d23e636ca432a6ce485ba0e41
Md Shakil Chowdhury <ms181161@gmail.com>
2021-12-12 15:07:06 +0600

    first commit

---
+++ src/containers/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+        <h2>T-4 Redux</h2>
+        <h2>Cart: 0</h2>
+    </div>
+  );
+};
+
+export default Header;
